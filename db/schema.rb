9ef1cb5e3c81b3afdb933c1e10006063751eafe9
Philippa Choong <pippachoong@gmail.com>
2022-09-17 12:17:27 +1000

    added events seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,26 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2022_09_16_074215) do
-=======
-ActiveRecord::Schema.define(version: 2022_09_16_073933) do
->>>>>>> d437f2e1e4fd015d390c2399518d6d68a0f83dce
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
   create_table "attractions", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "category_id"
-=======
+  end 
+
   create_table "destinations", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.text "image"
->>>>>>> d437f2e1e4fd015d390c2399518d6d68a0f83dce
     t.text "address"
     t.float "longitude"
     t.float "latitude"
@@ -37,20 +32,18 @@ ActiveRecord::Schema.define(version: 2022_09_16_073933) do
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
   create_table "categories", force: :cascade do |t|
     t.string "name"
-=======
+  end 
+  
   create_table "planners", force: :cascade do |t|
     t.string "name"
     t.text "image"
     t.integer "user_id"
->>>>>>> d437f2e1e4fd015d390c2399518d6d68a0f83dce
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-<<<<<<< HEAD
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
@@ -59,12 +52,12 @@ ActiveRecord::Schema.define(version: 2022_09_16_073933) do
     t.text "image"
     t.integer "destination_id"
     t.integer "attraction_id"
-=======
+  end 
+  
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
->>>>>>> d437f2e1e4fd015d390c2399518d6d68a0f83dce
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,130 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+e1 = Events.create!(
+    name: "Florence Paper Lantern Festival",
+    location:"Santissima Annunziata",
+    time: "4th September 2022, 8pm",
+    description: "On the evening of September 7th Florentines gather on the banks of the river Arno carrying colored paper lanterns. With lanterns on sticks they take part in the procession in honor of the Virgin Mary’s Nativity, that is celebrated the following day.",
+    image: "TEXT",
+    destination_id: d1.id, 
+    attraction_id: nil 
+)
+
+
+e2 = Events.create!(
+    name: "TEXT",
+    location:" TEXT ",
+    time: "4th September 2022, 8pm",
+    description: "TEXT",
+    image: "TEXT",
+    destination_id: nil, 
+    attraction_id: a1.id
+)
+
+e3 = Events.create!(
+    name: "Gion Odori",
+    location:"Gion",
+    time: "4th September 2022, 8pm",
+    description: "The Gion Higashi geisha district puts on their annual geisha dance in the Gion Kaikan hall on Higashioji-dori in early November. This is the smallest of the city is five geisha districts and their dance is both intimate and somewhat quaint – a stark contrast to the grand Miyako Odori held by the main Gion geisha district. This is part of the charm of the event. Also, this is the only major geisha dance to be held in the fall. ",
+    image: "TEXT",
+    destination_id: d1.id, 
+    attraction_id: nil 
+)
+
+
+e4 = Events.create!(
+    name: "TEXT",
+    location:" TEXT ",
+    time: "4th September 2022, 8pm",
+    description: "TEXT",
+    image: "TEXT",
+    destination_id: nil, 
+    attraction_id: a1.id
+)
+
+e5 = Events.create!(
+    name: "Rio Carnival",
+    location:"Rio",
+    time: "4th September 2022, 8pm",
+    description: "The Carnival in Rio de Janeiro is a festival held every year before Lent; it is considered the biggest carnival in the world, with two million people per day on the streets. The first Carnival festival in Rio occurred in 1723. It’s a wild celebration full of lively music, dancing, and extravagant costumes in the purpose-built Sambadrome.",
+    image: "TEXT",
+    destination_id: d1.id, 
+    attraction_id: nil 
+)
+
+
+e6 = Events.create!(
+    name: "TEXT",
+    location:" TEXT ",
+    time: "4th September 2022, 8pm",
+    description: "TEXT",
+    image: "TEXT",
+    destination_id: nil, 
+    attraction_id: a1.id
+)
+
+e7 = Events.create!(
+    name: "Shakespeare in the Park",
+    location:"Delacorte Theater",
+    time: "4th September 2022, 8pm",
+    description: "There’s nothing quite like hearing the Bard’s immortal words performed outside in New York, with a backdrop of natural splendor and the Belvedere Castle looming in the background like the world’s most impressive set decoration. Shakespeare in the Park’s popularity means that tickets aren’t easy to come by—but if you persevere, you can get seats.",
+    image: "TEXT",
+    destination_id: d1.id, 
+    attraction_id: nil 
+)
+
+
+e8 = Events.create!(
+    name: "TEXT",
+    location:" TEXT ",
+    time: "4th September 2022, 8pm",
+    description: "TEXT",
+    image: "TEXT",
+    destination_id: nil, 
+    attraction_id: a1.id
+)
+
+e9 = Events.create!(
+    name: "Garden Town Stellenbosch",
+    location:"Stellenbosch, Western Cape",
+    time: "20th October 2022, 9am",
+    description: "This 10-day long festival sees events and experiences taking place across Stellenbosch and includes a flower parade, presentations and installations from the Western Cape Association for Flower Arrangers, private home and estate show gardens and a range of workshops. From 22 October the Paul Roos Gymnasium hosts the AFGRI Spring Fair which features food, wine and fun activities for the kids.",
+    image: "TEXT",
+    destination_id: d1.id, 
+    attraction_id: nil 
+)
+
+
+e10 = Events.create!(
+    name: "TEXT",
+    location:" TEXT ",
+    time: "4th September 2022, 8pm",
+    description: "TEXT",
+    image: "TEXT",
+    destination_id: nil, 
+    attraction_id: a1.id
+)
+
+e11 = Events.create!(
+    name: "Shakespeare in the Park",
+    location:"Sahara Desert",
+    time: "4th September 2022, 8pm",
+    description: "Join a group of like-minded women 40+ for and exciting adventure in the Sahara Desert and High Atlas Mountains with Exodus Travels.The grandeur of the Sahara Desert is truly experienced on this trip as we journey from Marrakech across the High Atlas Mountains to the palm groves of the Draa Valley and the gateway to the desert at Zagora.",
+    image: "TEXT",
+    destination_id: d1.id, 
+    attraction_id: nil 
+)
+
+
+e12 = Events.create!(
+    name: "TEXT",
+    location:" TEXT ",
+    time: "4th September 2022, 8pm",
+    description: "TEXT",
+    image: "TEXT",
+    destination_id: nil, 
+    attraction_id: a1.id
+)
+
+
