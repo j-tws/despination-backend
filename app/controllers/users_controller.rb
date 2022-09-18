90871f652c932b3829c02857cc3bcc266beac7e1
Justin Tan <justintws10@gmail.com>
2022-09-18 16:56:41 +1000

    minor tweaks

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user 
 
   def current
-    render json: current user
+    render json: current_user
     # NOTE!! - Knock uses current_user NOT @current_user as we have used previously in a sessions controller. Ensure your code is typed as below.
   end
 
